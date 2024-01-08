3aa0163bdb78e67ec47f4702a82e3345639b9491
Vorem_dim <ustinov.artem2002@gmail.com>
2024-01-08 22:06:26 +0300

    Add buttons in main menu

--- src/commonMain/kotlin/Settings.kt
+++ src/commonMain/kotlin/Settings.kt
@@ -1,4 +1,5 @@
 import korlibs.audio.sound.*
+import korlibs.image.color.*
 import korlibs.image.font.*
 
 data class Settings(
@@ -6,6 +7,13 @@ data class Settings(
     val fonts: Map<String, Font>,
     val music: Map<String, Sound>,
     val sound: Map<String, Sound>,
+    val colors: Map<String, RGBA> = mapOf(
+        "MainText" to RGBA(127, 0, 255),
+        "CommonText" to RGBA(57, 0, 185),
+        "Unavailable" to RGBA(70, 70, 70),
+        "Button" to RGBA(0)
+    ),
+    var violetRose: Int = 0,
 
     // User Settings
     var musicVolume: Double = 1.0,
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,9 +10,10 @@ import korlibs.image.text.*
 import korlibs.korge.animate.*
 import korlibs.korge.input.*
 import korlibs.korge.view.align.*
+import korlibs.math.geom.*
 import korlibs.math.interpolation.*
 
-suspend fun main() = Korge() {
+suspend fun main() = Korge {
     val sceneContainer = sceneContainer()
     sceneContainer.changeTo { MainScene() }
 }
@@ -21,6 +22,8 @@ class MainScene : Scene() {
     private lateinit var settings: Settings
     private lateinit var background: Image
     private lateinit var textTitles: Array<Text>
+    private lateinit var buttons: Array<RoundRect>
+    private lateinit var textButtons: Array<Text>
     override suspend fun SContainer.sceneInit() {
         settings = Settings(
             mapOf(
@@ -45,7 +48,7 @@ class MainScene : Scene() {
         textTitles = arrayOf(
             text("Castle of the\nviolet rose").also {
                 it.textSize = 80.0
-                it.color = RGBA(127, 0, 255)
+                it.color = settings.colors["MainText"]!!
                 it.font = settings.fonts["Primary"]!!
                 it.alignment = TextAlignment.CENTER
                 it.alpha = 0.0
@@ -53,7 +56,7 @@ class MainScene : Scene() {
             },
             text("Tap to continue").also {
                 it.textSize = 30.0
-                it.color = RGBA(57, 0, 185)
+                it.color = settings.colors["CommonText"]!!
                 it.font = settings.fonts["Secondary"]!!
                 it.alignment = TextAlignment.CENTER
                 it.alpha = 0.0
@@ -61,6 +64,30 @@ class MainScene : Scene() {
             }
         )
         textTitles[1].y = textTitles[0].run { y + height }
+
+        buttons = Array(5) { index: Int ->
+            RoundRect(
+                Size(.3 * this.width, .1 * this.height),
+                RectCorners(30,15,15, 30)
+            ).also {
+                it.y = (.18 * (index - 2) + .45) * this.height
+                it.color = settings.colors["Button"]!!
+                it.alpha = .0
+            }.centerXOn(this)
+        }
+
+        val text = arrayOf("New game", "Continue", "Violet Rose", "Select hero", "Settings")
+        textButtons = Array(5) { index ->
+            text(text[index]).also {
+                it.textSize = 40.0
+                it.color = if (settings.violetRose != 3 && index == 2)
+                    settings.colors["Unavailable"]!!
+                else
+                    settings.colors["CommonText"]!!
+                it.font = settings.fonts["Primary"]!!
+                it.alpha = .0
+            }
+        }
     }
     override suspend fun SContainer.sceneMain() {
         settings.music["MainSound"]!!.play(PlaybackTimes.INFINITE)
@@ -70,13 +97,13 @@ class MainScene : Scene() {
         textTitles.forEach { text -> text.addTo(this) }
 
         animator {
-            parallel { alpha(background, 1.0, TimeSpan.fromSeconds(2), Easing.EASE_IN) }
-            parallel { alpha(textTitles[0], 1.0, TimeSpan.fromSeconds(2), Easing.EASE_IN) }
+            parallel { alpha(background, 1, TimeSpan.fromSeconds(2), Easing.EASE_IN) }
+            parallel { alpha(textTitles[0], 1, TimeSpan.fromSeconds(2), Easing.EASE_IN) }
         }.awaitComplete()
 
         var userTouch = true
         while(userTouch) {
-            val anim = blinking(textTitles[1], TimeSpan.fromSeconds(1)).awaitComplete()
+            blinking(textTitles[1], TimeSpan.fromSeconds(1)).awaitComplete()
             onClick {
                 userTouch = false
                 settings.sound["Select"]!!.play()
@@ -84,15 +111,23 @@ class MainScene : Scene() {
         }
 
         animator {
-            parallel { alpha(textTitles[1], 0.0, TimeSpan.fromSeconds(1), Easing.EASE_IN) }
-            parallel { alpha(textTitles[0], 0.0, TimeSpan.fromSeconds(2), Easing.EASE_IN) }
+            parallel { alpha(textTitles[1], .0, TimeSpan.fromSeconds(1), Easing.EASE_IN) }
+            parallel { alpha(textTitles[0], .0, TimeSpan.fromSeconds(2), Easing.EASE_IN) }
         }.awaitComplete()
         textTitles.forEach { text -> text.removeFromParent() }
+
+        buttons.forEach { button -> button.addTo(this) }
+        textButtons.forEachIndexed { index, textButton -> textButton.addTo(this).centerOn(buttons[index]) }
+
+        animator {
+            parallel { buttons.forEach { alpha(it, .5, TimeSpan.fromSeconds(1), Easing.EASE_IN_OUT) } }
+            parallel { textButtons.forEach { alpha(it, 1, TimeSpan.fromSeconds(1), Easing.EASE_IN_OUT) } }
+        }
     }
     private fun SContainer.blinking(text: Text, time: TimeSpan): Animator {
         val blink = animator()
         if (text.alpha == 1.0)
-            blink.sequence { alpha(text, 0.5, time, Easing.EASE_IN) }
+            blink.sequence { alpha(text, .5, time, Easing.EASE_IN) }
         else
             blink.sequence { alpha(text, 1, time, Easing.EASE_IN) }
         return blink
